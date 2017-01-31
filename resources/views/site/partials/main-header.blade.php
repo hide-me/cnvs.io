738a378d0bc8909d1cc37910edd67cefb4d4b499
Todd Austin <austin.todd.j@gmail.com>
2017-01-31 08:26:51 -0600

    :book: Main header copy update

--- resources/views/site/partials/main-header.blade.php
+++ resources/views/site/partials/main-header.blade.php
@@ -4,9 +4,9 @@
             <div class="col-md-6">
                 <h1 class="title wow fadeInUp">Simple, Powerful, Blog Publishing Platform</h1>
                 <p class="description wow fadeInUp" data-wow-delay=".2s">
-                    Write with unparalleled speed and efficiency in a clutter-free
-                    environment. Share your stories with the world on a beautiful template that
-                    didn't take 67 plugins to create. Oh, and did we mention it's free?
+                    Create your own blog with unparalleled speed and efficiency and share
+                    your stories with the world on a beautiful template that didn't take
+                    67 plugins to create. Oh, and did we mention it's free?
                 </p>
                 <br/>
                 <a href="https://www.youtube.com/watch?v=psu3pPdfYSM" class="popup-video btn btn-white btn-round btn-raised btn-lg wow fadeInUp" data-wow-delay=".4s">
