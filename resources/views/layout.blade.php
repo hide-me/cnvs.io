214473c63c8441b7b81483764376f38ca9d15bd8
Todd Austin <austin.todd.j@gmail.com>
2017-01-31 09:36:28 -0600

    Cleaned up asset paths

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,12 +4,12 @@
         @include('site.partials.meta')
         <title>Canvas - Simple, Powerful Blog Publishing Platform</title>
 
-        <link rel="shortcut icon" href="assets/img/ico/favicon.png">
+        <link rel="shortcut icon" href="{{ asset('assets/img/ico/favicon.png') }}">
 
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/img/ico/apple-touch-icon-144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/img/ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/img/ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="assets/img/ico/apple-touch-icon-57-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('assets/img/ico/apple-touch-icon-144-precomposed.png') }}">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('assets/img/ico/apple-touch-icon-114-precomposed.png') }}">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('assets/img/ico/apple-touch-icon-72-precomposed.png') }}">
+        <link rel="apple-touch-icon-precomposed" href="{{ asset('assets/img/ico/apple-touch-icon-57-precomposed.png') }}">
 
         @include('site.partials.css')
     </head>
