04be8832e066fc2081fde90e1f722208b1110b88
Todd Austin <austin.todd.j@gmail.com>
2017-02-27 16:50:31 -0600

    :label: Navbar alt tag update

--- resources/views/site/partials/navbar.blade.php
+++ resources/views/site/partials/navbar.blade.php
@@ -7,7 +7,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{ url('/') }}"><img src="{{ asset('assets/img/logo.png') }}" alt="Appy Logo" id="navbar-logo"></a>
+            <a class="navbar-brand" href="{{ url('/') }}"><img src="{{ asset('assets/img/logo.png') }}" alt="Canvas Logo" id="navbar-logo"></a>
         </div>
 
         <div class="collapse navbar-collapse" id="tt-navigation">
