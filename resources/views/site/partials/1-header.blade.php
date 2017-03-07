57e81510c82d918016c5c1438a826e60e57035bb
Todd Austin <austin.todd.j@gmail.com>
2017-03-07 06:43:05 -0600

    :fire: Removed fade in animation for headers

--- resources/views/site/partials/1-header.blade.php
+++ resources/views/site/partials/1-header.blade.php
@@ -2,8 +2,8 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6" id="main-header-content">
-                <h1 class="title wow fadeInUp">Simple, Powerful, Blog Publishing Platform</h1>
-                <p class="description wow fadeInUp" data-wow-delay=".2s">
+                <h1 class="title wow">Simple, Powerful, Blog Publishing Platform</h1>
+                <p class="description wow">
                     Create your own blog with unparalleled speed and efficiency and share
                     your stories with the world on a beautiful template that didn't take
                     67 plugins to create. Oh, and did we mention it's free?
@@ -12,15 +12,15 @@
                 {{--<a href="https://www.youtube.com/watch?v=psu3pPdfYSM" class="popup-video btn btn-white btn-round btn-raised btn-lg wow fadeInUp" data-wow-delay=".4s">--}}
                     {{--<i class="fa fa-play"></i>&nbsp;&nbsp;Watch the video--}}
                 {{--</a>--}}
-                <a href="http://cnvs.readme.io/docs" target="_blank" class="btn btn-white btn-round btn-raised wow fadeInUp" style=" font-weight: bold">
+                <a href="http://cnvs.readme.io/docs" target="_blank" class="btn btn-white btn-round btn-raised wow" style=" font-weight: bold">
                     <i class="fa fa-fw fa-rocket"></i>&nbsp;&nbsp;Get started
                 </a>
-                <a href="http://www.patreon.com/canvas" target="_blank" class="patreon-btn btn btn-round btn-raised wow fadeInUp" style="background-color: transparent; border: 1px solid white; color: white; font-weight: bold; margin: 0 15px 0 15px">
+                <a href="http://www.patreon.com/canvas" target="_blank" class="patreon-btn btn btn-round btn-raised wow" style="background-color: transparent; border: 1px solid white; color: white; font-weight: bold; margin: 0 15px 0 15px">
                     <img src="https://canvas-47a7.kxcdn.com/assets/img/patreon.png" id="patreon" style="width: 17px">&nbsp;&nbsp;Become a backer
                 </a>
             </div>
             <div class="col-md-6 text-right" id="header-image-container">
-                <img src="https://canvas-47a7.kxcdn.com/assets/img/header/blog-home.png" class="wow fadeInUp" id="header-image" data-wow-delay=".3s" alt="Hello World Blog Post" style="max-width: 420px;">
+                <img src="https://canvas-47a7.kxcdn.com/assets/img/header/blog-home.png" class="wow fadeInUp" id="header-image" alt="Hello World Blog Post" style="max-width: 420px;">
             </div>
         </div>
     </div>
