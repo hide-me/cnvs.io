62bb144f42ddd213a37ff37da71cfb8a66a27e9c
Todd Austin <austin.todd.j@gmail.com>
2017-03-09 13:15:04 -0600

    :fire: Removed footer navigation

--- resources/views/site/partials/11-footer.blade.php
+++ resources/views/site/partials/11-footer.blade.php
@@ -1,17 +1,7 @@
 <footer class="footer">
     <div class="container">
-        <br>
-        <nav class="text-center">
-            <ul>
-                <li><a href="https://cnvs.readme.io" target="_blank">Documentation</a></li>
-                <li><a href="https://cnvs.readme.io/blog" target="_blank">Blog</a></li>
-                <li><a href="https://github.com/cnvs/canvas" target="_blank">GitHub</a></li>
-                <li><a href="https://cnvs.hipchat.com" target="_blank">HipChat</a></li>
-                <li><a href="https://cnvs.readme.io/discuss" target="_blank">Support</a></li>
-            </ul>
-        </nav>
         <div class="copyright text-center small">
-            Released under the <a href="https://opensource.org/licenses/MIT" target="_blank">MIT License</a><br>Copyright &copy; {{ date('Y') }} <a href="https://toddaustin.io" target="_blank">Todd Austin</a>
+            Released under the <a href="https://opensource.org/licenses/MIT" target="_blank">MIT License</a><br>Copyright &copy; {{ date('Y') }} <a href="https://toddaustin.io" target="_blank">Todd Austin</a>. All rights reserved.
         </div>
     </div>
 </footer>
