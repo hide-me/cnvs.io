e1bdb814856d4f341ab3b8d90ab960cd1f2d0e7a
Todd Austin <austin.todd.j@gmail.com>
2017-04-21 16:06:48 -0500

    :link: Adds social icons in navbar

--- resources/views/site/partials/navbar.blade.php
+++ resources/views/site/partials/navbar.blade.php
@@ -14,9 +14,18 @@
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="https://cnvs.readme.io" target="_blank">Documentation</a></li>
                 <li><a href="https://cnvs.readme.io/blog" target="_blank">Blog</a></li>
-                <li><a href="https://github.com/cnvs/canvas" target="_blank">GitHub</a></li>
                 <li><a href="https://cnvs.hipchat.com" target="_blank">HipChat</a></li>
                 <li><a href="https://cnvs.readme.io/discuss" target="_blank">Support</a></li>
+                <li>
+                    <a href="https://github.com/cnvs/canvas" target="_blank" data-original-title="GitHub">
+                        <i class="fa fa-github"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/cnvs_io" target="_blank" data-original-title="Twitter">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                </li>
             </ul>
         </div>
     </div>
