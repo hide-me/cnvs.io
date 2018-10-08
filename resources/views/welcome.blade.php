0e8738452fd0a4de2ee21989340a68003a836508
Todd Austin <austin.todd.j@gmail.com>
2018-10-08 15:48:41 -0500

    Fixed the nav bar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,15 +11,17 @@
     </head>
     <body>
     <nav class="site-header sticky-top py-1">
-        <div class="container d-flex flex-column flex-md-row justify-content-between">
+        <div class="container d-flex flex-column flex-md-row">
             <a class="py-2" href="{{ url('/') }}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
             </a>
-            <a class="py-2 d-none d-md-inline-block" href="{{ url('https://docs.cnvs.io') }}">Documentation</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Blog</a>
-            <a class="py-2 d-none d-md-inline-block" href="{{ url('https://cnvs-io.slack.com') }}">Slack</a>
-            <a class="py-2 d-none d-md-inline-block" href="{{ url('https://twitter.com/cnvs_io') }}">Twitter</a>
-            <a class="py-2 d-none d-md-inline-block" href="{{ url('https://patreon.com/cnvs_io') }}">Patreon</a>
+            <div class="ml-auto">
+                <a class="d-none d-md-inline-block btn btn-link" href="{{ url('https://docs.cnvs.io') }}">Documentation</a>
+                <a class="d-none d-md-inline-block btn btn-link" href="#">Blog</a>
+                <a class="d-none d-md-inline-block btn btn-link" href="{{ url('https://cnvs-io.slack.com') }}">Slack</a>
+                <a class="d-none d-md-inline-block btn btn-link" href="{{ url('https://twitter.com/cnvs_io') }}">Twitter</a>
+                <a class="d-none d-md-inline-block btn btn-link" href="{{ url('https://patreon.com/cnvs_io') }}">Patreon</a>
+            </div>
         </div>
     </nav>
 
@@ -27,7 +29,7 @@
         <div class="col-md-5 p-lg-5 mx-auto my-5">
             <h1>Not just another blog</h1>
             <p class="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple's marketing pages.</p>
-            <a href="#" class="btn btn-lg btn-outline-secondary"><i class="fab fa-fw fa-github"></i> Github</a>
+            <a href="{{ url('https://github.com/cnvs/canvas') }}" class="btn btn-lg btn-outline-secondary"><i class="fab fa-fw fa-github"></i> Github</a>
         </div>
         <div class="product-device box-shadow d-none d-md-block"></div>
         <div class="product-device product-device-2 box-shadow d-none d-md-block"></div>
@@ -137,12 +139,13 @@
                 </ul>
             </div>
             <div class="col-6 col-md">
-                <h5>About</h5>
+                <h5>Sponsors</h5>
                 <ul class="list-unstyled text-small">
-                    <li><a class="text-muted" href="#">Team</a></li>
-                    <li><a class="text-muted" href="#">Locations</a></li>
-                    <li><a class="text-muted" href="#">Privacy</a></li>
-                    <li><a class="text-muted" href="#">Terms</a></li>
+                    <li><a class="text-muted" href="{{ url('https://www.araxis.com/') }}">Araxis</a></li>
+                    <li><a class="text-muted" href="{{ url('https://www.digitalocean.com/') }}">Digital Ocean</a></li>
+                    <li><a class="text-muted" href="{{ url('https://www.jetbrains.com/') }}">JetBrains</a></li>
+                    <li><a class="text-muted" href="{{ url('https://www.keycdn.com/') }}">KeyCDN</a></li>
+                    <li><a class="text-muted" href="{{ url('https://www.navicat.com/') }}">Navicat</a></li>
                 </ul>
             </div>
         </div>
