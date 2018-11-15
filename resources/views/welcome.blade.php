61dce1d5806354004a6f2066d4695b37ab910ec0
Nathan Johnson <nathancj224@gmail.com>
2018-11-14 19:04:24 -0500

    Changed it's to its on home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
         <div class="col-md-5 p-lg-5 mx-auto my-5">
-            <h1>Never judge a book by it's cover</h1>
+            <h1>Never judge a book by its cover</h1>
             <p class="lead font-weight-normal">As you may have noticed, things are amiss. Development on <b>Canvas v4.0</b> is in full swing, and it'll be the best one yet.</p>
             <a href="{{ url('https://github.com/cnvs/canvas') }}" class="btn btn-lg btn-outline-secondary"><i class="fab fa-fw fa-github"></i> GitHub</a>
         </div>
