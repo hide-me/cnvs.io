aad8e98fc4035225a5668ecee0c58581c2463992
Todd Austin <austin.todd.j@gmail.com>
2019-01-02 08:34:27 -0600

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="">
+    <meta name="og:type" content="website">
+    <meta name="og:title" content="{{ sprintf('%s — %s', config("app.name"), 'A Laravel publishing platform' ) }}">
+    <meta name="og:image" content="">
+    <meta name="og:site_name" content="{{ config("app.name") }}">
+    <meta name="og:description" content="">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="{{ sprintf('%s — %s', config("app.name"), 'A Laravel publishing platform' ) }}">
+    <meta name="twitter:image" content="">
+    <meta name="twitter:site" content="@cnvs">
+    <meta name="twitter:description" content="">
     <title>@yield('title')</title>
     <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
     <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
