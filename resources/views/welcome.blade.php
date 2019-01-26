e07d74f5ca1f861fb134f5af180de5ce2558b0f1
Todd Austin <austin.todd.j@gmail.com>
2019-01-26 06:36:30 -0600

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -104,7 +104,7 @@
     </div>
 
     <div class="container text-center mt-4 mb-5">
-        <p>Released under the <a href="{{ url('https://github.com/cnvs/canvas/blob/master/license') }}"
+        <p>Released under the <a href="{{ url('https://opensource.org/licenses/MIT') }}"
                                  class="font-weight-bold">MIT</a> license. <a
                     href="{{ url('https://github.com/cnvs/canvas') }}" class="font-weight-bold">View source</a>.</p>
         <p>Proudly hosted by <a href="{{ url('https://digitalocean.com/?refcode=41cb45b3c7db') }}"
