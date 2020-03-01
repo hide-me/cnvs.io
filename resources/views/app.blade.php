6210a04955d6a49edcbcd491de9cfa1e034b3c0e
Todd Austin <austin.todd.j@gmail.com>
2020-03-01 16:31:01 -0600

    updates

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,9 +21,6 @@
 
     <title>{{ sprintf('%s — %s', config('app.name'), trans('app.meta.title') ) }}</title>
 
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Karla|Merriweather:400,700">
-
     <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
 
     <link rel="icon" type="image/png" href="{{ asset('img/favicon.ico') }}">
@@ -31,12 +28,12 @@
 <body>
 <div class="mb-4 mt-3">
     <div class="px-0">
-        <div class="col-xl-8 offset-xl-2 col-md-12 px-0">
+        <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1 col-md-12 px-0">
             <nav class="navbar navbar-light justify-content-between flex-nowrap flex-row">
                 <a class="navbar-brand py-0 font-serif" href="/" aria-label="{{ sprintf('%s %s', config('app.name'), 'homepage' ) }}">
                     <svg data-name="Logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 110.981" width="30">
-                        <path class="secondary" d="M4.808 100.34c4.66-3.767 11.309-2.044 16.167.307 2.856 1.382 5.563 3.046 8.399 4.468a61.637 61.637 0 008.568 3.538c11.478 3.727 23.997 3.29 34.205-3.595a51.44 51.44 0 0013.321-13.613c1.1-1.592-1.498-3.094-2.59-1.515-6.486 9.38-15.876 17.099-27.622 18.026a43.71 43.71 0 01-18.16-2.759c-5.845-2.102-10.93-5.643-16.62-8.052-5.764-2.44-12.641-3.087-17.79 1.073-1.501 1.213.633 3.324 2.122 2.121zM21.8 66.342L5.422 74.049a.768.768 0 00-.417.503A44.368 44.368 0 01.025 93.85a.768.768 0 00.933.936 42.063 42.063 0 0119.623-4.983.768.768 0 00.511-.43l7.384-16.371a.768.768 0 00-.157-.86l-5.649-5.648a.768.768 0 00-.87-.151z"/>
-                        <path class="secondary" d="M96 13.467a13.424 13.424 0 01-4.76 10.271L40.434 67.334l-5.855 4.97a5.744 5.744 0 01-7.77-.324l-4.282-4.28a5.741 5.741 0 01-.282-7.813l6.282-7.24L72.363 4.643A13.466 13.466 0 0196 13.467z"/>
+                        <path class="fill-body-color" d="M4.808 100.34c4.66-3.767 11.309-2.044 16.167.307 2.856 1.382 5.563 3.046 8.399 4.468a61.637 61.637 0 008.568 3.538c11.478 3.727 23.997 3.29 34.205-3.595a51.44 51.44 0 0013.321-13.613c1.1-1.592-1.498-3.094-2.59-1.515-6.486 9.38-15.876 17.099-27.622 18.026a43.71 43.71 0 01-18.16-2.759c-5.845-2.102-10.93-5.643-16.62-8.052-5.764-2.44-12.641-3.087-17.79 1.073-1.501 1.213.633 3.324 2.122 2.121zM21.8 66.342L5.422 74.049a.768.768 0 00-.417.503A44.368 44.368 0 01.025 93.85a.768.768 0 00.933.936 42.063 42.063 0 0119.623-4.983.768.768 0 00.511-.43l7.384-16.371a.768.768 0 00-.157-.86l-5.649-5.648a.768.768 0 00-.87-.151z"/>
+                        <path class="fill-body-color" d="M96 13.467a13.424 13.424 0 01-4.76 10.271L40.434 67.334l-5.855 4.97a5.744 5.744 0 01-7.77-.324l-4.282-4.28a5.741 5.741 0 01-.282-7.813l6.282-7.24L72.363 4.643A13.466 13.466 0 0196 13.467z"/>
                         <path class="primary" d="M41.388 66.38l-6.808 5.923a5.744 5.744 0 01-7.77-.323l-4.283-4.28a5.741 5.741 0 01-.282-7.813l6.282-7.24z"/>
                     </svg>
                 </a>
@@ -73,7 +70,7 @@
 </div>
 
 <main class="py-4">
-    <div class="col-xl-8 offset-xl-2 col-md-12">
+    <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1 col-md-12">
         <div class="mb-5 text-md-left">
             <p class="text-muted text-uppercase mb-0 font-weight-bolder">@lang('app.header.subtext')</p>
             <h1 class="display-4 font-serif">@lang('app.header.title')</h1>
